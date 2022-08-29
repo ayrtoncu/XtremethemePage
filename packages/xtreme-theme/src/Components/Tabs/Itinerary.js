f4d76cd886e79f8ac02c7591664d8855035f147b
ayrtoncu <bc.ayrton@gmail.com>
2022-08-29 13:34:12 -0500

    #36 configuracion final de itinerario

--- packages/xtreme-theme/src/Components/Tabs/Itinerary.js
+++ packages/xtreme-theme/src/Components/Tabs/Itinerary.js
@@ -1,60 +1,70 @@
-import { connect, css, styled } from 'frontity'
-import React,{useState} from 'react';
-import iconOpen from '../../Icons/menuicons/expand_more_white_24dp.svg';
-import iconClose from '../../Icons/menuicons/expand_less_white_24dp.svg';
+import { connect, css, styled } from "frontity";
+import React, { useState } from "react";
+import iconOpen from "../../Icons/menuicons/expand_more_white_24dp.svg";
+import iconClose from "../../Icons/menuicons/expand_less_white_24dp.svg";
 
 const Itinerary = ({ itineraryData }) => {
-  const [subnav, setSubnav] = useState(false);
-
-  const showSubnav = () => setSubnav(!subnav);
   // console.log(itineraryData);
   return (
     <>
-      {itineraryData.map(function (item, index) {
-        console.log(item);
-        return (
-          <>{}</>
-        )
+      {itineraryData.map(function (dayitinerario, index) {
+        console.log(dayitinerario);
+        return <Slider dataDayItinerary={dayitinerario} />;
       })}
-      {/* <div>
-        <div css={sidebarLink} onClick={itineraryData.subNav && showSubnav}>
-          <div>
-            <SidebarLabel>{{__html: itineraryData[0].title_itinetary }}</SidebarLabel>
-          </div>
-          <div>
-            {itineraryData.subNav && subnav
-              ? iconOpen
-              : itineraryData.subNav
-              ? iconClose
-              : null}
-          </div>
-        </div>
-      </div> */}
-      {subnav &&
-        itineraryData.subNav.map((item, index) => {
-          return (
-            <Link css={dropdownLink} link={item.path} key={index}>
-              {/* {item.icon} */}
-              <SidebarLabel>{item.title}</SidebarLabel>
-            </Link>
-          );
-        })}
-      {/* <div dangerouslySetInnerHTML={{ __html: itineraryData[0].title_itinetary }}></div> */}
     </>
+  );
+};
+
+export default connect(Itinerary);
+
+//slider
+const Slider = ({ dataDayItinerary }) => {
+  const [subnav, setSubnav] = useState(false);
+
+  const showSubnav = () => setSubnav(!subnav);
+  return (
+    <div>
+      <div css={sidebarLink} onClick={dataDayItinerary.itinerario_content && showSubnav}>
+        <div>
+          {/* {item.icon} */}
+          <SidebarLabel>{dataDayItinerary.title_itinetary}</SidebarLabel>
+        </div>
+        <div>
+          {dataDayItinerary.itinerario_content && subnav
+            ? <IconClose/>
+            : dataDayItinerary.itinerario_content
+            ? <IconOpen/>
+            : null}
+        </div>
+      </div>
+      {subnav&&<div css={dropdownLink}>
+        {dataDayItinerary.itinerario_content}
+      </div>}
+    </div>
+  );
+};
+
+const IconClose = () => {
+  return (
+    <img src={ iconOpen} />
+  )
+}
+const IconOpen = () => {
+  return (
+    <img src={ iconClose} />
   )
 }
 
-export default connect(Itinerary);
 const SidebarLabel = styled.span`
   margin-left: 16px;
 `;
 let sidebarLink = css`
-  background-color: #008B39;
+  background-color: #fff;
   box-shadow: 0 2px 5px 0 rgb(0 0 0 / 8%), 0 2px 10px 0 rgb(0 0 0 / 2%);
   margin-top: 7px;
-  border-left: solid 5px #FFD000;
+  border-left: solid 5px #008b39;
   display: flex;
-  color: #fff;
+  color: #000;
   justify-content: space-between;
   align-items: center;
   padding: 30px 20px;
@@ -64,24 +74,22 @@ let sidebarLink = css`
   font-size: 18px;
 
   &:hover {
-    background: #FFD000;
-    border-left: 4px solid #632ce4;
     cursor: pointer;
   }
 `;
 
 let dropdownLink = css`
-  background: #414757;
-  height: 60px;
+  background-color: #fff;
+  height: auto;
   padding-left: 3rem;
   display: flex;
   align-items: center;
   text-decoration: none;
-  color: #f5f5f5;
+  color: #000;
   font-size: 18px;
+  border-left: solid 5px #008b39;
 
   &:hover {
-    background: #632ce4;
-    cursor: pointer;
+    // background: #632ce4;
   }
 `;
